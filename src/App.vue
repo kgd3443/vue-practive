501973c0956cac44803b662671b122b897a393fa
kgd3443 <shyshy7665@naver.com>
2025-11-08 19:59:01 +0900

    최종 완성

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,12 @@ import E10Ref from "@/components/example6/E-10-ref.vue";
 import E11Reactive from "@/components/example6/E-11-reactive.vue";
 import E12RefComponent from "@/components/example6/E-12-ref-component.vue";
 import E07OptionsApi from "@/components/example5/E-07-Options-API.vue";
+import E01Instance from "@/components/example1/E-01-instance.vue";
+import E02Reactive from "@/components/example1/E-02-reactive.vue";
+import E03Binding from "@/components/example1/E-03-binding.vue";
+import E04Directives from "@/components/example2/E-04-directives.vue";
+import E05ParentComponent from "@/components/example3/ParentComponent.vue";
+import E06ParentComponent from "@/components/example4/ParentComponent.vue";
 
 
 
